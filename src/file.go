1bf08397274e078c8b7411cb483d6e260171d398
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-06-23 23:41:39 -0500

    feat: find and append in go file

--- src/file.go
+++ src/file.go
@@ -1,9 +1,12 @@
 package main
 
+import "fmt"
+
 // my comment
 // second
 func Add(a uint8, b uint8) uint8 {
 	// add values a and b
+	fmt.Println("yay!")
 	return a + b
 }
 
--- src/main.go
+++ src/main.go
@@ -5,6 +5,7 @@ import (
 	"go/ast"
 	"go/parser"
 	"go/token"
+	"os"
 	"strings"
 )
 
@@ -14,9 +15,26 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	file_by_line := strings.Split(string(file), "\n")
+
 	for _, c := range parsed_res.Comments {
-		pos := c.Pos()
-		fmt.Printf("L%d: %s", fset.Position(pos).Line, c.Text())
+		if strings.TrimSpace(c.Text()) != "add values a and b" {
+			continue
+		}
+
+		line_number := fset.Position(c.Pos()).Line
+		file_by_line = append(
+			file_by_line[:line_number+1],
+			file_by_line[line_number:]...,
+		)
+		file_by_line[line_number] = "\tfmt.Println(\"yay!\")"
+		file_string := strings.Join(file_by_line, "\n")
+		os.WriteFile(filename, []byte(file_string), 0644)
 	}
 }
 
@@ -29,7 +47,7 @@ func get_parsed(filename string) (*token.FileSet, *ast.File, error) {
 func file_input() string {
 	fmt.Print("filename :: ")
 	var filename string
-	fmt.Scanln(&filename)
+	fmt.Scan(&filename)
 
 	if !strings.Contains(filename, ".go") {
 		panic("file have a .go extension")
