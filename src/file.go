12e2d269b0c6b84fe6ee678b709d8704690c4272
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-06-23 22:40:10 -0500

    feat: parse go file from input + get comments with line numbers

---
+++ src/file.go
@@ -0,0 +1,10 @@
+package main
+
+// my comment
+// second
+func Add(a uint8, b uint8) uint8 {
+	// add values a and b
+	return a + b
+}
+
+// third
--- src/main.go
+++ src/main.go
@@ -2,8 +2,37 @@ package main
 
 import (
 	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	filename := file_input()
+	fset, parsed_res, err := get_parsed(filename)
+	if err != nil {
+		panic(err)
+	}
+	for _, c := range parsed_res.Comments {
+		pos := c.Pos()
+		fmt.Printf("L%d: %s", fset.Position(pos).Line, c.Text())
+	}
+}
+
+func get_parsed(filename string) (*token.FileSet, *ast.File, error) {
+	fset := token.NewFileSet()
+	tokens, err := parser.ParseFile(fset, filename, nil, parser.ParseComments)
+	return fset, tokens, err
+}
+
+func file_input() string {
+	fmt.Print("filename :: ")
+	var filename string
+	fmt.Scanln(&filename)
+
+	if !strings.Contains(filename, ".go") {
+		panic("file have a .go extension")
+	}
+	return filename
 }
